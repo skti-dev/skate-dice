1d79e9f407b9bf1545084d862cdf4352711621b1
Augusto Seabra <augusto.seabra00@gmail.com>
2021-12-28 18:40:20 -0300

    Adding icon

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const Footer = () => {
   return (
     <footer className="footer">
-      <span> Desenvolvido por Augusto Seabra | 2021 </span>
+      <span> &#127756; Desenvolvido por Augusto Seabra | 2021 </span>
     </footer>
   )
 }
