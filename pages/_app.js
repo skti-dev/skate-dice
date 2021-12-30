65db9575dfadaf364c61124573437b050b398644
Augusto Seabra <augusto.seabra00@gmail.com>
2021-12-30 02:14:52 -0300

    Separating css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import "../assets/css/reset.css"
 import "../assets/css/default.css"
+import "../assets/css/title.css"
+import "../assets/css/dice.css"
+import "../assets/css/history.css"
+import "../assets/css/footer.css"
 
 export default function SkateDice({ Component, pageProps }) {
   return <Component {...pageProps} />
